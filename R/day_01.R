5442d39a7f72394d617f1515bd9c8cda8584a861
Darrin Speegle <speegled@slu.edu>
2020-12-02 12:04:50 -0600

    solution using loops

--- R/day_01.R
+++ R/day_01.R
@@ -6,3 +6,28 @@ outer(dd, dd, function(x, y) x * y)[ind[1]] #first star
 
 ind <- which(outer(outer(dd, dd, function(x, y) x + y), dd, function(x, y) x + y) == 2020)
 outer(outer(dd, dd, function(x, y) x * y), dd, function(x, y) x * y)[ind[1]] #second star
+
+
+#'
+#' Not sure why I decided to do this using outer. Maybe because I didn't have the 
+#' data in hand when I was working on it due to a glitch on day one. I think a more
+#' natural, but less R like, approach would have been:
+#'
+
+for(a in dd) {
+  for(b in dd) {
+    if(a + b == 2020) {
+      print(a * b)
+    }
+  }
+}
+
+for(a in dd) {
+  for(b in dd) {
+    for(c in dd) {
+      if(a + b + c == 2020) {
+        print(a * b * c)
+      }
+    }
+  }
+}
