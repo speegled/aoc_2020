859bb1afdf21030330c68f629edc34e37b67ba5b
Darrin Speegle <speegled@slu.edu>
2020-12-06 22:50:13 -0600

    day 6 redo

--- R/day_06.R
+++ R/day_06.R
@@ -38,3 +38,36 @@ ee %>%
   summarize(n = count_vals(value)) %>% 
   pull(n) %>% sum #second star!
 
+#'
+#' Re-do using the same idea but with separate_rows
+#'
+
+
+dd <- read_file("data/day_06")
+
+ee <- dd %>%
+  strsplit('\n\n')  %>% 
+  unlist() %>% 
+  strsplit('[ \n]') %>% 
+  melt() %>% 
+  group_by(L1) %>% 
+  mutate(num = n()) %>% 
+  separate_rows(value, sep = "") %>% 
+  filter(value != "")
+
+ee %>% 
+  group_by(L1) %>% 
+  summarize(n = length(unique(value))) %>% 
+  summarize(sum(n)) #first star
+
+ee %>% 
+  group_by(value, L1) %>% 
+  summarize(n = n(), num = num) %>%  
+  filter(n == num) %>% 
+  distinct(L1) %>% 
+  nrow() #second star
+  
+
+
+
+
