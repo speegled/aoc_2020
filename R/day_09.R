41efe383d5cf82d7471dbbcf4dd41bc32740ac11
Darrin Speegle <speegled@slu.edu>
2020-12-09 09:43:20 -0600

    day 9

---
+++ R/day_09.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(bit64)
+
+dd <- as.integer64(read_lines("data/day_09"))
+
+i <- 0
+while(dd[i + 26] %in% outer(dd[(i + 1):(25 + i)], dd[(1 + i):(25 + i)], "+")) {
+  i <- i + 1
+}
+dd[i + 26] #first star!
+
+ii <- i
+for(i in 1:477) {
+  for(j in (i + 1):478) {
+    x <- sum(dd[i:j])
+    if(x == dd[26 + ii]) {
+      print(sum(min(  dd[i:j] ), max(dd[i: j])))
+      break
+    } 
+  }
+} #second star!
