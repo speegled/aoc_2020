d15b8920d441fad358a797e5149701a728322e0f
Darrin Speegle <speegled@slu.edu>
2020-12-22 22:20:46 -0600

    day 22 improvement

--- R/day_22.R
+++ R/day_22.R
@@ -1,7 +1,7 @@
 library(collections)
 library(tidyverse)
 
-dd <- data.frame(x = read_lines("data/day_22"))
+dd <- data.frame(x = read_lines("../aoc_2020/data/day_22"))
 dd <- dd %>% mutate(player = cumsum(x == "") + 1) %>% 
   filter(!str_detect(x, "Player")) %>% 
   filter(x != "")
@@ -34,32 +34,22 @@ if(p2_curr > p1_curr) {
 (vals * (length(vals):1)) %>% sum() #first star!
 
 #'
-#' Hmm, this took about 3 hours on my machine, just as an FYI
+#' this next part takes about 5 minutes
 #'
 
-is_previous_game <- function(cg, ag) { #this is ridiculously slow!
-  N <- max(ag$game)
-  for(i in 1:N) {
-    one_game <- ag %>% 
-      filter(game == i) %>% 
-      select(cards, player)
-    if(identical(cg, one_game)) {
-      return(TRUE)
-    }
-  }
-  return(FALSE)
+is_previous_game <- function(cg, ag) {
+  return(cg %in% ag)
 }
 
 play_game <- function(p1, p2) {
-  all_games <- data.frame(cards = c(unlist(p1$as_list()), unlist(p2$as_list())),
-                          player = c(rep(1, p1$size()), rep(2, p2$size())),
-                          game = 1)
+  all_games <- digest::digest(list(p1, p2)) 
   while(p1$size() > 0 && p2$size() > 0) {
+    N <- 1
     c1 <- p1$peek()
     c2 <- p2$peek()
     if(c1 <= p1$size() - 1 && c2 <= p2$size() - 1) {
-
-      q1 <- queue(items = p1$as_list()[2:(p1$peek() + 1)])
+      
+      q1 <- queue(items = p1$as_list()[2:(p1$peek() + 1)]) #this is the slowdown, I think. wishing I hadn't used a queue
       q2 <- queue(items = p2$as_list()[2:(p2$peek() + 1)])
       
       recursive_result <- play_game(q1, q2)
@@ -81,25 +71,18 @@ play_game <- function(p1, p2) {
       }
     } #end of one round
     
-    current_game <- data.frame(cards = c(unlist(p1$as_list()), unlist(p2$as_list())),
-                               player = c(rep(1, p1$size()), rep(2, p2$size())))
+    current_game <- digest::digest(list(p1, p2))
     
-    N <- max(all_games$game) + 1
+    N <- length(all_games)
     if(N %% 1000 == 0) {
+      print(N)
       if(is_previous_game(current_game, all_games)) {
         return(list(winner = 1, p1 = p1, p2 = p2))
       }
     }
-    all_games <- bind_rows(all_games, 
-                           data.frame(cards = c(unlist(p1$as_list()), unlist(p2$as_list())),
-                            player = c(rep(1, p1$size()), rep(2, p2$size())),
-                            game = N))
-    if(N %% 200 == 0) {
-      print(N)
-    }
+    all_games <- c(all_games, current_game)
   }
-  print(ppp)
-  ppp <<- ppp + 1
+
   if(p1$size() == 0) { #player 2 wins
     return(list(winner = 2, p1, p2))
   } else{
@@ -112,5 +95,5 @@ if(vall$winner == 1) {
   ((vall[[2]]$as_list() %>% unlist()) * (vall[[2]]$size():1)) %>% sum()
 } else {
   ((vall[[3]]$as_list() %>% unlist()) * (vall[[3]]$size():1)) %>% sum()
-} #second star!
+}
 
